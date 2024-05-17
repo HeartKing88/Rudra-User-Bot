c7a9e3e09fbcee5b8bbedeaaded1d52238ea2844
Vikky yadav <146602351+Vikashya12@users.noreply.github.com>
2024-05-17 20:55:04 +0530

    Update func.py

--- Rudra/modules/func.py
+++ Rudra/modules/func.py
@@ -1,97 +1,36 @@
-from .. import call, db
-from typing import Union
-from pyrogram.types import Message
-from pytgcalls.types import *
-from pytgcalls.types.stream import *
+from pytgcalls import filters
+from pytgcalls.types import ChatUpdate, Update
+
+from . import queues
+from ..clients.clients import app, call
+from .streams import run_stream, close_stream
 
 
-async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
-    msg = (
-        message.edit_text
-        if bool(message.from_user and message.from_user.is_self or message.outgoing)
-        else (message.reply_to_message or message).reply_text
+async def run_async_calls():
+    @call.on_update(
+        filters.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT
+        )
     )
-    return await msg(*args, **kwargs)
-
-
-eor = edit_or_reply
-
-
-async def put_que(chat_id, file, type):
-    put = {
-        "chat_id": chat_id,
-        "file": file,
-        "type": type,
-    }
-    check = db.get(chat_id)
-    if not check:
-        db[chat_id] = []
-        db[chat_id].append(put)
-    else:
-        que = db[chat_id]
-        que.append(put)
-        return int(len(que)-1)
-
-
-
-async def kicked_handler(_, chat_id: int):
-    try:
-        check = db.get(chat_id)
-        if check:
-            return check.pop(0)
-        return
-    except:
-        pass
-
-
-@call.on_closed_voice_chat()
-async def closed_voice_chat_handler(_, chat_id: int):
-    try:
-        check = db.get(chat_id)
-        if check:
-            return check.pop(0)
-        return
-    except:
-        pass
-
-
-@call.on_left()
-async def left_handler(_, chat_id: int):
-    try:
-        check = db.get(chat_id)
-        if check:
-            return check.pop(0)
-        return
-    except:
-        pass
-
-
-@call.on_stream_end()
-async def stream_end_handler(_, update: Update):
-    chat_id = update.chat_id
-    try:
-        check = db.get(chat_id)
-        if check:
-            que = db[chat_id]
-            que.pop(0)
-            if len(que) == 0:
-                db.pop(chat_id)
-                return await call.leave_group_call(chat_id)
-            else:
-                file = check[0]["file"]
-                type = check[0]["type"]
-                if type == "Audio":
-                    stream = AudioPiped(
-                        file,
-                        HighQualityAudio(),
-                    )
-                elif type == "Video":
-                    stream = AudioVideoPiped(
-                        file,
-                        HighQualityAudio(),
-                        HighQualityVideo(),
-                    )
-                return await call.change_stream(chat_id, stream)
-        return
-    except Exception as e:
-        print(f"**Error:** `{e}`")
+    @call.on_update(
+        filters.chat_update(
+            ChatUpdate.Status.KICKED | ChatUpdate.Status.LEFT_GROUP,
+        ),
+    )
+    async def stream_services_handler(_, update: Update):
+        return await close_stream(update.chat_id)
+    
+    
+    @call.on_update(filters.stream_end)
+    async def stream_end_handler(_, update: Update):
+        chat_id = update.chat_id
+        queues.task_done(chat_id)
+        if queues.is_empty(chat_id):
+            return await close_stream(chat_id)
+        check = queues.get(chat_id)
+        file = check["file"]
+        type = check["type"]
+        stream = await run_stream(file, type)
+        return await call.play(chat_id, stream)
+    
+        
